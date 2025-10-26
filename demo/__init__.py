1bc783093aee624c45cea8c96d3b0bc7f1f9c862
kim-hangyeol <taylor4359@naver.com>
2025-10-26 22:44:49 +0900

    docs: update demo package docstring

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,1 +1,8 @@
-"""Test suite for kcloud-cost-estimator"""
+"""
+Demo and test suite for kcloud-cost-estimator
+
+This package contains demonstration code and test cases for:
+- Energy prediction models
+- Calibration tools
+- API usage examples
+"""
